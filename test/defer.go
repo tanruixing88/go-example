156f1655d4d7f9182fa88f834c5098faf9d4cef8
tanruixing88 <tanruixing88@github.com>
2022-06-25 10:28:51 +0800

    add defer note

--- test/defer.go
+++ test/defer.go
@@ -12,13 +12,15 @@ func deferFuncParam1() {
 	return
 }
 
+// 返回结果值是0,因为返回值没有声明为i，是一个匿名变量，在返回前固定为0, 然后再执行defer，虽然将i值改为1
+// 但是匿名变量的值还为0，返回结果仍为0
 func foo1() int {
 	i := 0
 	defer func() { i++ } ()
 	return i
 }
 
-//返回值声明不带参数名则并不修改返回值
+//defer 处理已经固定下来是10,传入了函数后输出10和11,但返回值声明不带参数名，还是一个匿名变量，直接输出为4
 func foo2() int {
 	i := 10
 	defer func(i int) {
@@ -30,7 +32,8 @@ func foo2() int {
 	return i
 }
 
-//返回值声明带参数名同样并不修改返回值
+//返回值声明带参数名i说明返回值没有匿名变量直接为i，虽然后面i值为4,但是全部处理完返回前还得执行一下defer流程
+//但是defer执行的是值拷贝，改的不是返回的i，是拷贝的i，故结果还是4
 func foo3() (i int) {
 	i = 10
 	defer func(i int) {
